b229bd6e9364cf5683fd9161bc9574f251432471
Wen Bo Xie <wenbox323@gmail.com>
2017-07-06 14:54:57 -0400

    Add squares to the game canvas

--- prok.js
+++ prok.js
@@ -1,21 +1,12 @@
+import Game from './lib/game';
+import GameView from './lib/game_view';
+
 document.addEventListener('DOMContentLoaded', () => {
   const gameCanvas = document.getElementById('game-canvas');
   gameCanvas.width = window.innerWidth;
   gameCanvas.height = window.innerHeight;
-
   const ctx = gameCanvas.getContext('2d');
+  const game = new Game();
 
-  window.addEventListener('resize', resizeCanvas, false);
-
-  function resizeCanvas() {
-    gameCanvas.width = window.innerWidth;
-    gameCanvas.height = window.innerHeight;
-
-    gamePlay();
-  }
-
-  resizeCanvas();
-
-  function gamePlay() {
-  }
+  return new GameView(game, ctx).start();
 });
