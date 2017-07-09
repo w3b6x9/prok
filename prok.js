4ef0c1aefedd0f2f703e4cb6c1159dda8e6d443c
Wen Bo Xie <wenbox323@gmail.com>
2017-07-09 17:46:08 -0400

    Add game canvas border and limit game area

--- prok.js
+++ prok.js
@@ -3,11 +3,10 @@ import GameView from './lib/game_view';
 
 document.addEventListener('DOMContentLoaded', () => {
   const gameCanvas = document.getElementById('game-canvas');
-  gameCanvas.width = window.innerWidth;
-  gameCanvas.height = window.innerHeight;
+  gameCanvas.width = window.innerWidth - 200;
+  gameCanvas.height = window.innerHeight - 150;
 
   const ctx = gameCanvas.getContext('2d');
   const game = new Game();
-
-  return new GameView(game, ctx).start();
+  return new GameView(game, ctx).start(gameCanvas);
 });
